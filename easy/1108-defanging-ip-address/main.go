1d784ce7def8ee5cdb378396a448ff32604c5d2d
Vivek V <iamvivekv1996@yahoo.in>
2020-11-07 01:20:31 +0530

    Leetcode Problem #1108: Defanging IP Address, added a cleaner approach

--- easy/1108-defanging-ip-address/main.go
+++ easy/1108-defanging-ip-address/main.go
@@ -1,9 +1,15 @@
 // Problem: https://leetcode.com/problems/defanging-an-ip-address/
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func defangIPaddr(address string) string {
+	return strings.Replace(address, ".", "[.]", -1) // standard library, cleaner solution
+
+	/* my own solution, probably dirtier xP
 	defanged := make([]rune, 0)
 
 	for i := 0; i < len(address); i++ {
@@ -12,10 +18,11 @@ func defangIPaddr(address string) string {
 			temp = "[.]"
 		}
 
-		defanged = append(defanged, []rune(temp)...)
-	}
+			defanged = append(defanged, []rune(temp)...)
+		}
 
 	return string(defanged)
+	*/
 }
 
 func main() {
