1ca9480d0d6f29f6e1b3ec40c4a052672328851e
Vivek V <iamvivekv1996@yahoo.in>
2020-11-08 16:53:21 +0530

    Leetcode Problem #1486: Bitwise XOR Array

---
+++ easy/1486-bitwise-xor-array/main.go
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/xor-operation-in-an-array/
+package main
+
+func xorOperation(n int, start int) int {
+	nums := make([]int, n, n)
+	output := 0
+
+	for i := 0; i < n; i++ {
+		nums[i] = start + 2*i
+		output ^= nums[i]
+	}
+
+	return output
+}
---
+++ easy/1486-bitwise-xor-array/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import "testing"
+
+func Test_xorOperation(t *testing.T) {
+	type args struct {
+		n     int
+		start int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "first case",
+			args: args{
+				n:     5,
+				start: 0,
+			},
+			want: 8,
+		},
+		{
+			name: "second case",
+			args: args{
+				n:     4,
+				start: 3,
+			},
+			want: 8,
+		},
+		{
+			name: "third case",
+			args: args{
+				n:     1,
+				start: 7,
+			},
+			want: 7,
+		},
+		{
+			name: "fourth case",
+			args: args{
+				n:     10,
+				start: 5,
+			},
+			want: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := xorOperation(tt.args.n, tt.args.start); got != tt.want {
+				t.Errorf("xorOperation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
