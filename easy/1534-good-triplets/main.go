5c63269ca2e08e6b9f49d2fcc5348b42ad57cf4f
Vivek V <iamvivekv1996@yahoo.in>
2020-11-09 17:47:40 +0530

    Leetcode Problem #1534: Good Triplets, Reduced a few computations

--- easy/1534-good-triplets/main.go
+++ easy/1534-good-triplets/main.go
@@ -18,11 +18,12 @@ func countGoodTriplets(arr []int, a int, b int, c int) int {
 
 	for i := 0; i < len(arr); i++ {
 		for j := i + 1; j < len(arr); j++ {
-			for k := j + 1; k < len(arr); k++ {
-				if abs(arr[i]-arr[j]) <= a &&
-					abs(arr[j]-arr[k]) <= b &&
-					abs(arr[i]-arr[k]) <= c {
-					count++
+			if abs(arr[i]-arr[j]) <= a { // This saves a few computations but doesn't change the Time complexity.
+				for k := j + 1; k < len(arr); k++ {
+					if abs(arr[j]-arr[k]) <= b &&
+						abs(arr[i]-arr[k]) <= c {
+						count++
+					}
 				}
 			}
 		}
