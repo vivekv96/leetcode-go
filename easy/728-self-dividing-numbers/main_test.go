e748e2c6ba97c65c535d59bad854500df3c69aef
Vivek V <iamvivekv1996@yahoo.in>
2020-11-13 23:56:06 +0530

    Leetcode Problem #728: Self Dividing Numbers

---
+++ easy/728-self-dividing-numbers/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_selfDividingNumbers(t *testing.T) {
+	type args struct {
+		left  int
+		right int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "first case",
+			args: args{
+				left:  1,
+				right: 22,
+			},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := selfDividingNumbers(tt.args.left, tt.args.right); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("selfDividingNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
